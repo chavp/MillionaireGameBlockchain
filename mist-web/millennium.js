ba509a27d22d002c2d6b304393b556e59ded0576
Paul Lee <chaiyapong@3dsinteractive.com>
2016-11-27 15:44:09 +0700

    Refactor code.

--- mist-web/millennium.js
+++ mist-web/millennium.js
@@ -1,6 +1,15 @@
 
 function init() {
 
+    setupEthereum();
+    setupUi();
+}
+
+var allowAnswer = false;
+var millionaireContractABI = [ { "constant": false, "inputs": [], "name": "nextQuestion", "outputs": [], "payable": true, "type": "function" }, { "constant": false, "inputs": [ { "name": "_answer", "type": "string" } ], "name": "answer", "outputs": [], "payable": false, "type": "function" }, { "inputs": [], "type": "constructor" }, { "anonymous": false, "inputs": [ { "indexed": false, "name": "p", "type": "address" }, { "indexed": false, "name": "id", "type": "uint256" }, { "indexed": false, "name": "q", "type": "string" }, { "indexed": false, "name": "c", "type": "string" }, { "indexed": false, "name": "a", "type": "string" }, { "indexed": false, "name": "reward", "type": "uint256" } ], "name": "print_question", "type": "event" }, { "anonymous": false, "inputs": [ { "indexed": false, "name": "p", "type": "address" }, { "indexed": false, "name": "correctCount", "type": "uint256" }, { "indexed": false, "name": "msg", "type": "string" } ], "name": "print_result", "type": "event" }, { "anonymous": false, "inputs": [ { "indexed": true, "name": "from", "type": "address" }, { "indexed": true, "name": "to", "type": "address" }, { "indexed": false, "name": "value", "type": "uint256" } ], "name": "print_transfer", "type": "event" } ];
+var millionaireContractAddress = '0x71829240266681928426522D64f369d1D12Bc2d4';
+
+function setupEthereum(){
     // Checks Web3 support
     if(typeof web3 !== 'undefined' && typeof Web3 !== 'undefined') {
         // If there's a web3 library loaded, then make your own web3
@@ -32,16 +41,8 @@ function init() {
         document.getElementById("vote4").style.display = "none";
         document.getElementById("add-account").style.display = "block";
     }
-
-    setupUi();
 }
 
-var allowAnswer = false;
-
-var millionaireContractABI = [ { "constant": false, "inputs": [], "name": "nextQuestion", "outputs": [], "payable": true, "type": "function" }, { "constant": false, "inputs": [ { "name": "_answer", "type": "string" } ], "name": "answer", "outputs": [], "payable": false, "type": "function" }, { "inputs": [], "type": "constructor" }, { "anonymous": false, "inputs": [ { "indexed": false, "name": "p", "type": "address" }, { "indexed": false, "name": "id", "type": "uint256" }, { "indexed": false, "name": "q", "type": "string" }, { "indexed": false, "name": "c", "type": "string" }, { "indexed": false, "name": "a", "type": "string" }, { "indexed": false, "name": "reward", "type": "uint256" } ], "name": "print_question", "type": "event" }, { "anonymous": false, "inputs": [ { "indexed": false, "name": "p", "type": "address" }, { "indexed": false, "name": "correctCount", "type": "uint256" }, { "indexed": false, "name": "msg", "type": "string" } ], "name": "print_result", "type": "event" }, { "anonymous": false, "inputs": [ { "indexed": true, "name": "from", "type": "address" }, { "indexed": true, "name": "to", "type": "address" }, { "indexed": false, "name": "value", "type": "uint256" } ], "name": "print_transfer", "type": "event" } ];
-var millionaireContractAddress = '0x71829240266681928426522D64f369d1D12Bc2d4';
-
-//event print_question(address p, uint id, string q, string c, string a, uint reward);
 function setupUi(){
 
     document.getElementById('answer').addEventListener('click', function(){ nextQuestion(); }, false);
@@ -51,38 +52,6 @@ function setupUi(){
     document.getElementById('vote4').addEventListener('click', function(){ answer('d'); }, false);
 
     showQuestion(false);
-}
-
-function showQuestion(show){
-
-    var visibility = 'hidden';
-    if(show){
-        visibility = 'visible';
-    }
-    document.getElementById('proposal').style.visibility = visibility;
-    document.getElementById('vote1').style.visibility = visibility;
-    document.getElementById('vote2').style.visibility = visibility;
-    document.getElementById('vote3').style.visibility = visibility;
-    document.getElementById('vote4').style.visibility = visibility;
-}
-
-function showChoices(show){
-    var visibility = 'hidden';
-    if(show){
-        visibility = 'visible';
-    }
-    document.getElementById('vote1').style.visibility = visibility;
-    document.getElementById('vote2').style.visibility = visibility;
-    document.getElementById('vote3').style.visibility = visibility;
-    document.getElementById('vote4').style.visibility = visibility;
-}
-
-function showNext(show){
-    var visibility = 'hidden';
-    if(show){
-        visibility = 'visible';
-    }
-    document.getElementById('answer').style.visibility = visibility;
 }
 
 var millionaireContract = web3.eth.contract(millionaireContractABI);
@@ -195,55 +164,34 @@ function showLoading(show){
     }
 }
 
-//var greetingContractABI = [ { "constant": false, "inputs": [ { "name": "msg", "type": "string" } ], "name": "greeting", "outputs": [ { "name": "", "type": "string" } ], "payable": false, "type": "function" }, { "anonymous": false, "inputs": [ { "indexed": false, "name": "s", "type": "string" } ], "name": "print_log", "type": "event" } ];
-//var greetingContractAddress = '0x1ce3470C92595B094007355E5BfA3Fb20aD1cE92';
-function greeting() {
+function showQuestion(show){
 
-    /*console.log('greeting', web3.eth.accounts.length);
-
-    web3.eth.defaultAccount = web3.eth.accounts[0];
-
-    var greetingContract = web3.eth.contract(greetingContractABI);
-    var greetingFunction = greetingContract.at(greetingContractAddress);
-
-    var printLog = greetingFunction.print_log();
-    // Wait for the events to be loaded
-    printLog.watch(function(error, result){
-        document.getElementById('status').textContent = result.args.s;
-        console.log('event', result.args.s);
-    })
-
-    var result = greetingFunction.greeting('super hello world');
-    console.log('greeting result', result);
-    */
-}
-
-/*
-function vote(support) {
-    console.log('vote', web3.eth.accounts.length);
-
-    // Check if there are accounts available
-    if (web3.eth.accounts && web3.eth.accounts.length > 0) {
-
-        // Create a dialog requesting the transaction
-        ethervote.vote(proposalHash, support, {from: web3.eth.accounts[0]})
-        document.getElementById('status').textContent = 'Waiting for new block...';
-
-    } else {
-        console.log('callbacks', mist.callbacks);
-        mist.requestAccount(function(e, account) {
-            console.log('return account', e, account);
-            if(!e) {
-                // Create a dialog requesting the transaction
-                ethervote.vote(proposalHash, support, {from: account.toLowerCase()})
-                document.getElementById('status').textContent = 'Waiting for new block...';
-            }
-        });
-        console.log('callbacks', mist.callbacks);
-
+    var visibility = 'hidden';
+    if(show){
+        visibility = 'visible';
     }
-
-    document.getElementById("results").style.opacity = "1";
-    document.getElementById("see-results").style.opacity = "0";
+    document.getElementById('proposal').style.visibility = visibility;
+    document.getElementById('vote1').style.visibility = visibility;
+    document.getElementById('vote2').style.visibility = visibility;
+    document.getElementById('vote3').style.visibility = visibility;
+    document.getElementById('vote4').style.visibility = visibility;
 }
-    */
+
+function showChoices(show){
+    var visibility = 'hidden';
+    if(show){
+        visibility = 'visible';
+    }
+    document.getElementById('vote1').style.visibility = visibility;
+    document.getElementById('vote2').style.visibility = visibility;
+    document.getElementById('vote3').style.visibility = visibility;
+    document.getElementById('vote4').style.visibility = visibility;
+}
+
+function showNext(show){
+    var visibility = 'hidden';
+    if(show){
+        visibility = 'visible';
+    }
+    document.getElementById('answer').style.visibility = visibility;
+}
